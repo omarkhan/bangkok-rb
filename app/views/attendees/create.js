4fab816e6602e81c671a00a461c3be599401f4f1
skogman <yim.apichai@gmail.com>
2015-05-06 17:24:09 +0700

    fix map bugs and added form functionality with back-end

---
+++ app/views/attendees/create.js
@@ -0,0 +1,6 @@
+$(function () {
+  var $formContainer = $('#form-container');
+  $formContainer.children('form').fadeOut();
+  $formContainer.html('<p>Thanks for your submission fellow rubyist <br> We look forward to meeting you!</p>');
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :attendees
   root 'visitors#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
