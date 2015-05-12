969ffbefe6bef05e018ae6e15207ccea7320aa7c
skogman <yim.apichai@gmail.com>
2015-05-12 14:33:21 +0700

    redid migrations

---
+++ db/migrate/20150512073208_readd_occupation_and_phone_number_to_attendees.rb
@@ -0,0 +1,8 @@
+class ReaddOccupationAndPhoneNumberToAttendees < ActiveRecord::Migration
+  def change
+    # remove_column :attendees, :occupation, :string
+    # remove_column :attendees, :phone_number, :string
+    add_column :attendees, :occupation, :string
+    add_column :attendees, :phone_number, :string
+  end
+end
