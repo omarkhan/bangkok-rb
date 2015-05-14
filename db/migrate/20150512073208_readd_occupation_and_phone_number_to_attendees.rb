1be72c62c18d9693ce0b3bd7e9df6abdbc76ba13
skogman <yim.apichai@gmail.com>
2015-05-14 16:06:41 +0700

    Add Robin C avatar

--- db/migrate/20150512073208_readd_occupation_and_phone_number_to_attendees.rb
+++ db/migrate/20150512073208_readd_occupation_and_phone_number_to_attendees.rb
@@ -1,7 +1,7 @@
 class ReaddOccupationAndPhoneNumberToAttendees < ActiveRecord::Migration
   def change
-    # remove_column :attendees, :occupation, :string
-    # remove_column :attendees, :phone_number, :string
+    remove_column :attendees, :occupation, :string
+    remove_column :attendees, :phone_number, :string
     add_column :attendees, :occupation, :string
     add_column :attendees, :phone_number, :string
   end
