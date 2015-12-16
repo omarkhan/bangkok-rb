1675a78cdee46dca16ed48106c33302c254cfc55
Omar Khan <omar@omarkhan.me>
2015-12-16 18:47:14 +0700

    Add vagrant dev environment

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20150512073208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "attendees", force: :cascade do |t|
     t.string   "first_name"
