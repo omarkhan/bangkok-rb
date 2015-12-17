fd3820df22f02633d8578740d9eb8760ecaf623c
Omar Khan <omar@omarkhan.me>
2015-12-17 15:48:56 +0700

    Whitelist virtualbox host for web console

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Allow access to web console from virtualbox host
+  config.web_console.whitelisted_ips = '10.0.2.2'
 end
